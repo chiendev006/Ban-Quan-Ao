798a1156f225e209d9da88370b1d962111c905aa
chiennm <chiennmpp03120@gmail.com>
2024-11-28 16:05:51 +0700

    chuc nang gio hang

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 date_default_timezone_set('Asia/Ho_Chi_Minh');
 $base_url = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER["REQUEST_URI"] . '?') . '/';
 if (isset($_GET['action']) && $_GET['action'] != '') {
@@ -6,7 +7,13 @@ if (isset($_GET['action']) && $_GET['action'] != '') {
     switch ($action) {
         case "index-shop":
             include_once 'controller/shop/index.php';
-            break;  
+            break;
+        case "themgiohang" :
+            include_once "controller/cart/add.php" ;
+            break;
+        case "listgiohang" :
+            include_once "controller/cart/index.php" ;
+            break;
     } 
 }
  else {
---
+++ view/cart/index.php
@@ -0,0 +1,80 @@
+<?php
+include_once "view/layout/header.php";
+?>
+<div class="container-fluid pt-5">
+        <div class="row px-xl-5">
+            <div class="col-lg-8 table-responsive mb-5">
+                <table class="table table-bordered text-center mb-0">
+                    <thead class="bg-secondary text-dark">
+                        <tr>
+                            <th>Products</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody class="align-middle">
+                        <?php  
+                            foreach ($listGioHang as $item) {
+                        ?>
+                        <tr>
+                            <td class="align-middle"><img src="<?= $base_url . 'admin/upload/' . $item['img'] ?>" alt="" style="width: 55%; height: 100px;"> <?php $item['ten_sp'] ?></td>
+                            <td class="align-middle"><?= number_format($item['gia_sp']) ?> VĐN</td>
+                            <td class="align-middle">
+                                <div class="input-group quantity mx-auto" style="width: 100px;">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-sm btn-primary btn-minus" >
+                                        <i class="fa fa-minus" value=""></i>   
+                                        </button>
+                                    </div>
+                                    <input type="text" class="form-control form-control-sm bg-secondary text-center" value="<?= $item['so_luong'] ?>">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-sm btn-primary btn-plus">
+                                            <i class="fa fa-plus"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="align-middle"><?= number_format($item['gia_sp'] * $item['so_luong']) ?> VNĐ</td>
+                        </tr>
+                        <?php }?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-lg-4">
+                <form class="mb-5" action="">
+                    <div class="input-group">
+                        <input type="text" class="form-control p-4" placeholder="Coupon Code">
+                        <div class="input-group-append">
+                            <button class="btn btn-primary">Apply Coupon</button>
+                        </div>
+                    </div>
+                </form>
+                <div class="card border-secondary mb-5">
+                    <div class="card-header bg-secondary border-0">
+                        <h4 class="font-weight-semi-bold m-0">Cart Summary</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between mb-3 pt-1">
+                            <h6 class="font-weight-medium">Subtotal</h6>
+                            <h6 class="font-weight-medium">$150</h6>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <h6 class="font-weight-medium">Shipping</h6>
+                            <h6 class="font-weight-medium">$10</h6>
+                        </div>
+                    </div>
+                    <div class="card-footer border-secondary bg-transparent">
+                        <div class="d-flex justify-content-between mt-2">
+                            <h5 class="font-weight-bold">Total</h5>
+                            <h5 class="font-weight-bold">$160</h5>
+                        </div>
+                        <button class="btn btn-block btn-primary my-3 py-3">Proceed To Checkout</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+include_once "view/layout/footer.php";
+?>
